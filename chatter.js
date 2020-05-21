c2ee2913522aa388df2bb2d81672572f951d8cf1
rdoyle99 <rpdoyle1@gmail.com>
2020-05-20 20:49:46 -0400

    first iteration of url to iframe

--- chatter.js
+++ chatter.js
@@ -2,8 +2,7 @@ window.onload = function() {
   const queryString = window.location.search;
   var title = document.getElementById("title")
   title.innerHTML = queryString
+  const link = queryString.substr(1)
+  const frame = document.getElementById("contentFrame")
+  frame.src = link
 }
-
-const queryString = window.location.search;
-const link = queryString.substr(1)
-alert(link);
