e3dd01a3dcbc5a3b14bf6e05e61cba470abd6ead
rdoyle99 <rpdoyle1@gmail.com>
2020-05-20 20:55:32 -0400

    iframe is working, edited size" "
    "
    "

--- chatter.js
+++ chatter.js
@@ -1,7 +1,5 @@
 window.onload = function() {
   const queryString = window.location.search;
-  var title = document.getElementById("title")
-  title.innerHTML = queryString
   const link = queryString.substr(1)
   const frame = document.getElementById("contentFrame")
   frame.src = link
